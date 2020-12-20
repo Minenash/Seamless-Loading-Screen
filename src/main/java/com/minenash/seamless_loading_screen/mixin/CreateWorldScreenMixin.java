0d6dd756ed53b9da447ccbd761055abe379d9cc1
minenash <minenash@protonmail.com>
2020-12-20 17:30:10 -0500

    Fixed new worlds not saving screenshot properly the first time

---
+++ src/main/java/com/minenash/seamless_loading_screen/mixin/CreateWorldScreenMixin.java
@@ -0,0 +1,21 @@
+package com.minenash.seamless_loading_screen.mixin;
+
+import com.minenash.seamless_loading_screen.ScreenshotLoader;
+import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CreateWorldScreen.class)
+public class CreateWorldScreenMixin {
+
+    @Shadow private String saveDirectoryName;
+
+    @Inject(method = "createLevel", at = @At("HEAD"))
+    private void getWorldName(CallbackInfo _info) {
+        ScreenshotLoader.setScreenshot(saveDirectoryName);
+    }
+
+}
