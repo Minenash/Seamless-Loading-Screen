648be15c7930539f9fafdc9e52658fc107ba34da
minenash <minenash@protonmail.com>
2021-01-01 19:32:37 -0500

    Fixed dependency and directory already exists error

--- src/main/java/com/minenash/seamless_loading_screen/SeamlessLoadingScreen.java
+++ src/main/java/com/minenash/seamless_loading_screen/SeamlessLoadingScreen.java
@@ -3,12 +3,10 @@ package com.minenash.seamless_loading_screen;
 import com.minenash.seamless_loading_screen.config.ConfigManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,7 +24,7 @@ public class SeamlessLoadingScreen implements ClientModInitializer {
         try {
             Path path = FabricLoader.getInstance().getGameDir().resolve("screenshots/worlds");
             Files.createDirectories(path.resolve("singleplayer"));
-            Files.createDirectory(path.resolve("servers"));
+            Files.createDirectories(path.resolve("servers"));
         } catch (IOException e) {
             e.printStackTrace();
         }
