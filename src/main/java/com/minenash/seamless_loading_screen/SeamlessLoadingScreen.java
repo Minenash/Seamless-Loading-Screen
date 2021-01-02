b20df1d2a107703ad3d65c4c270b7e48f4bec841
minenash <minenash@protonmail.com>
2021-01-01 22:56:28 -0500

    Fixed keybind not being registered

--- src/main/java/com/minenash/seamless_loading_screen/SeamlessLoadingScreen.java
+++ src/main/java/com/minenash/seamless_loading_screen/SeamlessLoadingScreen.java
@@ -4,6 +4,7 @@ import com.minenash.seamless_loading_screen.config.ConfigManager;
 import com.minenash.seamless_loading_screen.config.ConfigScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -17,7 +18,10 @@ public class SeamlessLoadingScreen implements ClientModInitializer {
 
     public static boolean changeWorldJoinScreen = false;
 
-    public static final KeyBinding OPEN_SETTINGS = new KeyBinding("seamless_loading_screen.keybind.config", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, "key.categories.misc");
+    public static final KeyBinding OPEN_SETTINGS = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "seamless_loading_screen.keybind.config",
+            InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G,
+            "key.categories.misc"));
 
     @Override
     public void onInitializeClient() {
