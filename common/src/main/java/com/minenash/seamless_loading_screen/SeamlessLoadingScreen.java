7e4349848fc0e4aa77dbd130bccb4ab421faf1b9
Dragon-Seeker <timekeeperguild@gmail.com>
2023-08-27 18:12:54 -0500

    Fix Various Bugs
    - Fix issue with declaring a high version of Fabric Loader
    - Attempt to fix Forge Port of Sodium
    - Fix crash if resizing the window before blur shader object is created

--- common/src/main/java/com/minenash/seamless_loading_screen/SeamlessLoadingScreen.java
+++ common/src/main/java/com/minenash/seamless_loading_screen/SeamlessLoadingScreen.java
@@ -17,12 +17,10 @@ public class SeamlessLoadingScreen {
 
     public static boolean changeWorldJoinScreen = false;
 
-    public static ScreenshotLoader.BlurHelper BLUR_PROGRAM;
+    public static ScreenshotLoader.BlurHelper BLUR_PROGRAM = new ScreenshotLoader.BlurHelper();
 
     public static void onInitializeClient() {
         Config.load();
-
-        SeamlessLoadingScreen.BLUR_PROGRAM = new ScreenshotLoader.BlurHelper();
 
         try {
             Path path = PlatformFunctions.getGameDir().resolve("screenshots/worlds");
