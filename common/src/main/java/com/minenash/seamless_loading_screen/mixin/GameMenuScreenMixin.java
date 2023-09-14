8adfa44091595f3c3b97caa918b782e97bf128aa
Dragon-Seeker <timekeeperguild@gmail.com>
2023-09-14 14:19:35 -0500

    Adjust Mixin Target for Forge Reimplementing Ingame Mod Menu

--- common/src/main/java/com/minenash/seamless_loading_screen/mixin/GameMenuScreenMixin.java
+++ common/src/main/java/com/minenash/seamless_loading_screen/mixin/GameMenuScreenMixin.java
@@ -7,11 +7,14 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(GameMenuScreen.class)
 public class GameMenuScreenMixin {
 
-    @ModifyArg(method = "initWidgets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;builder(Lnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)Lnet/minecraft/client/gui/widget/ButtonWidget$Builder;", ordinal = 1), index = 1)
+    @ModifyArg(method = "initWidgets", slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;isInSingleplayer()Z")),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;builder(Lnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)Lnet/minecraft/client/gui/widget/ButtonWidget$Builder;", ordinal = 0),
+            index = 1)
     private ButtonWidget.PressAction adjust(ButtonWidget.PressAction onPress){
         return button -> {
             OnLeaveHelper.beginScreenshotTask(() -> {
